754fd4104946e1aa2a3c5d007ab55e6e192ba397
Alexey Drozdov <daju@yandex.ru>
2017-06-06 15:05:20 +0300

    Fix error while loading historyStore

---
+++ src/helper.js
@@ -0,0 +1,32 @@
+var _createClass = function() {
+    function defineProperties(target, props) {
+        for (var i = 0, descriptor; i < props.length; i++) {
+            descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || !1;
+            descriptor.configurable = !0;
+            if ("value" in descriptor) descriptor.writable = !0;
+            Object.defineProperty(target, descriptor.key, descriptor);
+        }
+    }
+    return function(Constructor, protoProps, staticProps) {
+        if (protoProps) defineProperties(Constructor.prototype, protoProps);
+        if (staticProps) defineProperties(Constructor, staticProps);
+        return Constructor;
+    };
+}();
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+
+function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
+    }
+}
+
+module.exports.createClass = _createClass;
+module.exports.interopRequireDefault = _interopRequireDefault;
+module.exports.classCallCheck = _classCallCheck;
--- src/stores/historyStore.js
+++ src/stores/historyStore.js
@@ -1,21 +1,23 @@
 var alt = require('../alt');
 var historyActions = require('../actions/historyActions');
 
+var helper = require('../helper');
 
-class historyStore {
-    constructor() {
+var historyStore = function() {
+    function historyStore() {
+        helper.classCallCheck(this, historyStore);
         this.bindActions(historyActions);
 
         this.history = false;
     }
-
-    onHistory(history) {
-        this.setState({
-            history: history
-        });
-    }
-
-
-}
-
+    helper.createClass(historyStore, [{
+        key: 'onHistory',
+        value: function onHistory(history) {
+            this.setState({
+                history: history
+            });
+        }
+    }]);
+    return historyStore;
+}();
 module.exports = alt.createStore(historyStore);
